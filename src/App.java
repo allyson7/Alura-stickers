7e582070a9cfc461eef4f59f46f275073e059dd5
allyson7 <allysonlopes0@gmail.com>
2022-07-25 17:31:33 -0400

    :construction: Creating the http request

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        // Fazer uma conexão HTTP e buscar os top 250 filmes do IMDB
+        String url = "https://api.mocki.io/v2/549a5d8b/Top250Movies";
+
+        URI endereco = URI.create(url);
+
+        var client = HttpClient.newHttpClient();
+
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        // Extrair só os dados que interessam (título, poster, classificação)
+
+        // Exibir e manipular os dados do nosso jeito
     }
 }
